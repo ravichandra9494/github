09d01f477e3d353d757db84389811baa03f51d24
ravi <krcece14@gmail.com>
2020-03-10 10:05:31 +0530

    commiting by person2 for firsttime

---
+++ src/newpackage/Person2.java
@@ -0,0 +1,13 @@
+package newpackage;
+
+public class Person2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("im the person who added this calss after getting from remote git hub");
+//github--person2
+//gitnew--person1
+System.out.println("gitnew start you code from here after u download from remote github after i push");
+	}
+
+}
